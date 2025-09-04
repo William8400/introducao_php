1b5dfc241b4811e430bdebc6a03e9ed81728eb80
William8400 <wdonato02@gmail.com>
2025-09-04 16:14:56 -0300

    ..

--- ola_mundo.php
+++ ola_mundo.php
@@ -1,15 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <?php
+    echo "<title>Introdução ao PHP</title>"
+    ?>
+    
 
-// comentar em linha 
+</head>
+<body>
 
-/* 
-
-    Comentário em bloco
-
-
-*/
-// echo é para passar uma mensagem para tela
-
-echo "Olá mundo";
-
-?>
+    
+</body>
+</html>
