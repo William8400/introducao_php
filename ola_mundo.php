60dc44a43e978acb33e8427cb89bd14e8926b274
William8400 <wdonato02@gmail.com>
2025-09-04 16:19:27 -0300

    Introdução  ao php

--- ola_mundo.php
+++ ola_mundo.php
@@ -4,13 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <?php
-    echo "<title>Introdução ao PHP</title>"
+    <?=
+     "<title>Introdução ao PHP</title>";
     ?>
     
 
 </head>
 <body>
+
+    <?php
+        echo "<h1> Titulo </h1>";
+    ?>
 
     
 </body>
