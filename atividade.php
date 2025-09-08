fa85d29a969af4bc6117f7aaacd861ed2e573f82
William8400 <wdonato02@gmail.com>
2025-09-08 13:34:20 -0300

    Trocando e colocando parágrafos

--- atividade.php
+++ atividade.php
@@ -18,13 +18,13 @@ $hobby = ["Jogar Quadribol", "Explorar Hogwarts", "Passar tempo com os amigos", 
 
 echo "<h1> Nomes dos Bruxos de hogwarts</h1>";
 
-echo "<h2> Nome: $nome</h2>";
-echo "<h2> Idade: $idade</h2>";
-echo "<h2> Especie: $especie</h2>";
-echo "<h2>Vivo: " . ($vivo ? "Sim" : "Não") . "</h2>";
-echo "<h2> Altura: $altura</h2>";
+echo "<p> Nome: $nome</p>";
+echo "<p> Idade: $idade</p>";
+echo "<p> Especie: $especie</p>";
+echo "<p>Vivo: " . ($vivo ? "Sim" : "Não") . "</p>";
+echo "<p> Altura: $altura</p>";
 
-echo "<h2> hobby principal: $hobby[0]</h2>";
+echo "<p> hobby principal: $hobby[0]</p>";
 
 
 $nome2 = "Hermione Granger";
@@ -35,12 +35,12 @@ $altura2 = 1.71;
 // Nivel danoninho com array indexado
 $hobby2 = ["Ler livros", "Estudar/Aprender Feitiços", "defender causas casuais", "ajudar os outros","Planejar e Organizar"];
 
-echo "<h2> Nome: $nome2</h2>";
-echo "<h2> Idade: $idade2</h2>";
-echo "<h2> Especie: $especie2</h2>";
-echo "<h2>Vivo: " . ($vivo2 ? "Sim" : "Não") . "</h2>";
-echo "<h2> Altura: $altura2</h2>";
-echo "<h2> hobby principal: $hobby2[1]</h2>";
+echo "<p> Nome: $nome2 </p>";
+echo "<p>Idade: $idade2 </p> ";
+echo "<p>Especie: $especie2 </p>";
+echo "<p>Vivo: " . ($vivo2 ? "Sim" : "Não") . " </p> ";
+echo "<p>Altura: $altura2 </p> ";
+echo "<p>hobby principal: $hobby2[1]</p>";
 
 $nome3 = "Ron Weasley";
 $idade3 = 37;
@@ -50,13 +50,13 @@ $altura3 = 1.85;
 // Nivel danoninho com array indexado
 $hobby3 = ["Jogar xadrez bruxo", "Quadribol", "Comer", "Passar tempo com os amigos", "Evitar Estudar"];
 
-echo "<h2> Nome: $nome3</h2>";
-echo "<h2> Idade: $idade3</h2>";
-echo "<h2> Especie: $especie3</h2>";
-echo "<h2>Vivo: " . ($vivo3 ? "Sim" : "Não") . "</h2>";
-echo "<h2> Altura: $altura3</h2>";
+echo "<p> Nome: $nome3</p>";
+echo "<p> Idade: $idade3</p>";
+echo "<p> Especie: $especie3</p>";
+echo "<p>Vivo: " . ($vivo3 ? "Sim" : "Não") . "</p>";
+echo "<p> Altura: $altura3</p>";
 
-echo "<h2> hobby principal: $hobby3[0]</h2>";
+echo "<p> hobby principal: $hobby3[0]</p>";
 
 
 
